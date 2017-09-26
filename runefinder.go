950c36320fa53cad92a8d8e7952338e329b8fa34
Centeno <centeno@diegocenteno.com>
2017-09-26 18:58:52 -0300

    Passo-02

--- runefinder.go
+++ runefinder.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"io"
 	"strconv"
 	"strings"
 )
@@ -9,4 +11,9 @@ func AnalisarLinha(ucdLine string) (rune, string) {
 	campos := strings.Split(ucdLine, ";") 
 	código, _ := strconv.ParseInt(campos[0], 16, 32)
 	return rune(código), campos[1]
+}
+
+func Listar(texto io.Reader, consulta string) {
+	runa, nome := '?', "QUESTION MARK"
+	fmt.Printf("U+%04X\t%[1]c\t%s\n", runa, nome)
 }
--- runefinder_test.go
+++ runefinder_test.go
@@ -1,9 +1,21 @@
 package main
 
-import "testing"
+import (
+	"testing"
+	"strings"
+)
 
 const linhaLetraA = "0041;LATIN CAPITAL LETTER A;Lu;0;L;;;;;N;;;;0061;"
 
+const linhas3Da43 = `
+003D;EQUALS SIGN;Sm;0;ON;;;;;N;;;;;
+003E;GREATER-THAN SIGN;Sm;0;ON;;;;;Y;;;;;
+003F;QUESTION MARK;Po;0;ON;;;;;N;;;;;
+0040;COMMERCIAL AT;Po;0;ON;;;;;N;;;;;
+0041;LATIN CAPITAL LETTER A;Lu;0;L;;;;;N;;;;0061;
+0042;LATIN CAPITAL LETTER B;Lu;0;L;;;;;N;;;;0062;
+0043;LATIN CAPITAL LETTER C;Lu;0;L;;;;;N;;;;0063;
+`
 func TestAnalisarLinha(t *testing.T) {
 	runa, nome := AnalisarLinha(linhaLetraA)
 	if runa != 'A' {
@@ -13,4 +25,10 @@ func TestAnalisarLinha(t *testing.T) {
 	if nome != nomeA {
 		t.Errorf("Esperava %q, veio %q", nomeA, nome)
 	}
+}
+
+func ExampleListar() {
+	texto := strings.NewReader(linhas3Da43)
+	Listar(texto, "MARK")
+	// Output: U+003F	?	QUESTION MARK
 }
